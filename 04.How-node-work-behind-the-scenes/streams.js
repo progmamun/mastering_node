20ca7df58d69e7dcd2496a6506da239f67b73619
progmamun <mamun333089@gmail.com>
2022-04-26 21:32:46 +0600

    streams

---
+++ 04.How-node-work-behind-the-scenes/streams.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const server = require('http').createServer();
+
+server.on('request', (req, res) => {
+  const readable = fs.createReadStream('test-file.txt');
+  readable.pipe(res);
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening..');
+});
