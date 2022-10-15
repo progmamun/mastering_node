aefcd23222f26b3f3667d1bdbdd52bd9f0b22cc9
progmamun <mamun333089@gmail.com>
2022-10-15 13:27:50 +0600

    server-connect-package-install

---
+++ 08-section/server.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const color = require('colors');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+const app = require('./app');
+const connectDB = require('./config/db');
+
+// Connect to database
+connectDB();
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
