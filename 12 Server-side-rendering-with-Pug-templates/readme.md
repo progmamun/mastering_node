81684a42d2a2c8d9176110ba6498f2fb2afa89ec
progmamun <mamun333089@gmail.com>
2023-01-08 12:08:33 +0600

    some-package

--- 12 Server-side-rendering-with-Pug-templates/readme.md
+++ 12 Server-side-rendering-with-Pug-templates/readme.md
@@ -1,3 +1,8 @@
 ## npm package
 
 - `npm i pug`
+- `npm install --save-dev parcel`
+- `npm install axios`
+- `npm i @babel/polyfill`
+- ``
+- ``
--- 12 Server-side-rendering-with-Pug-templates/routes/viewRoutes.js
+++ 12 Server-side-rendering-with-Pug-templates/routes/viewRoutes.js
@@ -4,6 +4,7 @@ const viewController = require('../controllers/viewsController');
 const router = express.Router();
 
 router.get('/', viewController.getOverview);
-router.get('/tour', viewController.getTour);
+router.get('/tour/:slug', viewController.getTour);
+router.get('/login', viewController.getLoginForm);
 
 module.exports = router;
